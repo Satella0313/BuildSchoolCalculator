a6274331b65e6110bfc99a32f183000a43377546
heng <a123944852@gmail.com>
2020-03-16 15:05:50 +0800

    計算

--- Mylibrary/Class1.cs
+++ Mylibrary/Class1.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Mylibrary
 {
-    public class Class1
+    public abstract class Math
     {
+        public double x { get; set; }
+        public double y { get; set; }
+        public abstract double GetArea();
+    }
+    public class Add : Math
+    {
+        public override double GetArea()
+        {
+            return x + y;
+        }
+    }
+    public class Minus : Math
+    {
+        public override double GetArea()
+        {
+            return x - y;
+        }
+    }
+    public class Multiply : Math
+    {
+        public override double GetArea()
+        {
+            return x * y;
+        }
+    }
+    public class Division : Math
+    {
+        public override double GetArea()
+        {
+            return x / y;
+        }
     }
 }
